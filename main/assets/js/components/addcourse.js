836011dcfbb7bfa0a1c4d70ee1126c267fe49079
WingLim <643089849@qq.com>
2020-03-20 21:22:00 +0800

    feat: change weekday judge

--- main/assets/js/components/addcourse.js
+++ main/assets/js/components/addcourse.js
@@ -1,56 +1,56 @@
-let insert = (weekday, html) => {
-    ul = document.getElementById(weekday)
+function insert (id, html) {
+    ul = document.getElementById(id)
     ul.insertAdjacentHTML('beforeend', html)
 }
-let create = (start, end, title, info) => {
+
+function findplace(weekday, ele){
+    switch (weekday) {
+        case '周一':
+            insert('1', ele)
+            break;
+        case '周二':
+            insert('2', ele)
+            break;
+        case '周三':
+            insert('3', ele)
+            break;
+        case '周四':
+            insert('4', ele)
+            break;
+        case '周五':
+            insert('5', ele)
+            break;
+        case '周六':
+            insert('6', ele)
+            break;
+        case '周日':
+            insert('7', ele)
+            break;
+    }
+}
+function create (course) {
     range = parseInt(Math.random() * 3 +1)
+    time = course.timeinfo
+    info = JSON.stringify(course)
     html = `<li class="cd-schedule__event">
-        <a data-start="${start}" data-end="${end}" href="#0" data-content="${info}" data-event="event-${range}">
-            <em class="cd-schedule__name">${title}</em>
+        <a data-start="${time.start}" data-end="${time.end}" href="#0" data-content='${info}' data-event="event-${range}">
+            <em class="cd-schedule__name">${course.title}</em>
            
         </a>
         </li>`
     return html
 }
 
+
 axios.get('https://api.limxw.com/schedule/json/18011317')
     .then(function (resp) {
         r = resp
         resp.data.forEach(course => {
-            time = course.timeinfo
-            info = `${course.teacher}+${time.week.start}+${time.week.end}+${time.week.flag}+${course.location}`
-            ele = create(
-                time.start,
-                time.end,
-                course.name,
-                info
-            )
-            switch (time.weekday) {
-                case 1:
-                    insert('1', ele)
-                    break;
-                case 2:
-                    insert('2', ele)
-                    break;
-                case 3:
-                    insert('3', ele)
-                    break;
-                case 4:
-                    insert('4', ele)
-                    break;
-                case 5:
-                    insert('5', ele)
-                    break;
-                case 6:
-                    insert('6', ele)
-                    break;
-                case 7:
-                    insert('7', ele)
-                    break;
-            }
+            let ele = create(course)
+            findplace(course.timeinfo.weekday, ele)
         });
         renderSchedule()
     })
     .catch(function (err) {
         console.log(err)
-    })
+    });
