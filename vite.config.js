7dd74214f8027909ffb8c8339d38af4594c79c24
WingLim <643089849@qq.com>
2021-05-30 18:38:46 +0800

    fix: process is not defined

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { windi } from 'svelte-windicss-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     svelte({
       preprocess: [
