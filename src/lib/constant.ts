1b09b468b235479cc384b957fe93f429e8805ea7
WingLim <643089849@qq.com>
2021-05-30 18:41:31 +0800

    feat: move select items to constant

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -14,3 +14,27 @@ export const times = [
   {start: '19:20', end: '20:05'},
   {start: '20:10', end: '20:55'},
 ]
+
+export const propertyItems = [
+  {value: '学科必修', label: '学科必修', group: '必修课'},
+  {value: '专业必修', label: '专业必修', group: '必修课'},
+  {value: '通识必修', label: '通识必修', group: '必修课'},
+  {value: '实践必修', label: '实践必修', group: '必修课'},
+  {value: '课外必修', label: '课外必修', group: '必修课'},
+  {value: '英语必修', label: '英语必修', group: '必修课'},
+  {value: '科技发展与科学精神', label: '科技发展与科学精神', group: '公选课'},
+  {value: '国际视野与公民教育', label: '国际视野与公民教育', group: '公选课'},
+  {value: '人文素质与艺术修养', label: '人文素质与艺术修养', group: '公选课'},
+  {value: '专业选修', label: '专业选修', group: '选修课'},
+  {value: '通识选修', label: '通识选修', group: '选修课'},
+]
+
+export const weekdayItems = [
+  {value: '周一', label: '周一'},
+  {value: '周二', label: '周二'},
+  {value: '周三', label: '周三'},
+  {value: '周四', label: '周四'},
+  {value: '周五', label: '周五'},
+  {value: '周六', label: '周六'},
+  {value: '周日', label: '周日'},
+]
