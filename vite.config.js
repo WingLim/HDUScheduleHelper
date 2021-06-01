841a8ede26e69d0c9e36ed4604d8373346049e28
WingLim <643089849@qq.com>
2021-06-01 20:31:44 +0800

    chore: typescript support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
+import autopreprocess from "svelte-preprocess";
 import { windi } from 'svelte-windicss-preprocess';
+
+const preprocess = autopreprocess({})
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +13,8 @@ export default defineConfig({
   plugins: [
     svelte({
       preprocess: [
-        windi({})
+        preprocess,
+        windi({}),
       ]
     }),
   ]
