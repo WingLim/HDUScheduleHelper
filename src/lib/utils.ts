dd271b09a476b5b0c5ab155d7247c81c0f6fce71
WingLim <643089849@qq.com>
2021-06-01 23:35:41 +0800

    fix: add weekend

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,9 @@
-import { times, weekdays } from './constant'
+import { times, weekdays, weekend } from './constant'
 import { boolSaveToLocal, courses, idMap } from './store'
 import { get_store_value } from 'svelte/internal'
+import { Writable } from 'svelte/store'
 
-export function getPosition(start, end, weekday) {
+export function getPosition(start: string, end: string, weekday: string) {
   let startIndex = 0
   let endIndex = 0
   let weekdayIndex = 0
@@ -13,7 +14,7 @@ export function getPosition(start, end, weekday) {
       endIndex = index + 3
     }
   })
-  weekdays.forEach((item, index) => {
+  weekdays.concat(weekend).forEach((item, index) => {
     if (item == weekday) {
       weekdayIndex = index + 2
     }
@@ -33,5 +34,92 @@ export function saveToLocal() {
 
     let idMapStr = JSON.stringify(Array.from(get_store_value(idMap).entries()))
     localStorage.setItem('idMap', idMapStr)
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function getPositionString(position) {
+  let str = ''
+  str += position.row_start
+  str += position.row_end
+  str += position.col_start
+  return str
+}
+
+function updateWritable(write: Writable<any>) {
+  write.update(write => write)
+}
+
+
+function $<T>(item: Writable<T>): T {
+  return get_store_value(item)
+}
+
+export function addCourse(course) {
+  let id = course.class_id
+  let infos = course.time_info
+
+  let conflict = false
+  var conflictKeys = []
+  
+  infos.forEach(info => {
+    let position = getPosition(info.start, info.end, info.weekday)
+    let key = getPositionString(position)
+    let tmpStart = parseInt(key[0], 10)
+    let tmpEnd = parseInt(key[1], 10)
+    let count =  tmpEnd - tmpStart
+    if (count == 3) {
+      let tmpKey = String(tmpStart) + String(tmpEnd - 1) + key[2]
+      if ($(courses).has(tmpKey)) {
+        conflictKeys.push(tmpKey)
+        conflict = true
+      }
+    } else if (count == 2) {
+      let tmpKey = String(tmpStart) + String(tmpEnd + 1) + key[2]
+      if ($(courses).has(tmpKey)) {
+        conflictKeys.push(tmpKey)
+        conflict = true
+      }
+    }
+    let tmpKey = String(tmpStart - 1) + String(tmpEnd) + key[2]
+    if ($(courses).has(tmpKey)) {
+      conflictKeys.push(tmpKey)
+      conflict = true
+    }
+    if ($(courses).has(key)) {
+      conflictKeys.push(key)
+      conflict = true
+    }
+  })
+
+  if (conflict) {
+    conflictKeys.forEach(key => {
+      $(courses).get(key).warn = true
+      updateWritable(courses)
+
+      sleep(500).then(() => {
+        if ($(courses).has(key)) {
+          $(courses).get(key).warn = false
+          updateWritable(courses)
+        }
+      })
+    })
+    
+  } else {
+    let keys = []
+    infos.forEach(info => {
+      let position = getPosition(info.start, info.end, info.weekday)
+      let key = getPositionString(position)
+      keys.push(key)
+      let options = {id: id, course: course, timeInfo: info, position: position, warn: false}
+      $(courses).set(key, options)
+      updateWritable(courses)
+    })
+    $(idMap).set(id, keys)
+    updateWritable(idMap)
+    saveToLocal()
   }
 }
